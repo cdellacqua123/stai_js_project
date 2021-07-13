914a77d160beed566152ff3c61c2a8139df98ef6
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-07-13 12:56:51 -0700

    Got graph starting to work again, still need to work on adding SMA & getting rid of blank weekend dates

--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -34,56 +34,55 @@ export function chartData(data) {
             type: 'line',
             data: []
         }]
+        ,
+        chart: {
+            height: 350,
+            type: 'line',
+        },
+        title: {
+            text: 'CandleStick Chart',
+            align: 'left'
+        },
+        stroke: {
+            width: [3, 1]
+        },
+        tooltip: {
+            shared: true,
+            custom: [function ({ seriesIndex, dataPointIndex, w }) {
+                return w.globals.series[seriesIndex][dataPointIndex]
+            }, function ({ seriesIndex, dataPointIndex, w }) {
+                var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
+                var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
+                var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
+                var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
+                return (
+                    ''
+                )
+            }]
+        },
+        xaxis: {
+            type: 'datetime',
+        },
+        annotations: {
+            xaxis: [
+                {
+                    x: 'Oct 06 14:00',
+                    borderColor: '#00E396',
+                    label: {
+                        borderColor: '#00E396',
+                        style: {
+                            fontSize: '12px',
+                            color: '#fff',
+                            background: '#00E396'
+                        },
+                        orientation: 'horizontal',
+                        offsetY: 7,
+                        text: 'Annotation Test'
+                    }
+                }
+            ]
+        },
     };
-        // ,
-        // chart: {
-        //     height: 350,
-        //     type: 'line',
-        // },
-        // title: {
-        //     text: 'CandleStick Chart',
-        //     align: 'left'
-        // },
-        // stroke: {
-        //     width: [3, 1]
-        // },
-        // tooltip: {
-        //     shared: true,
-        //     custom: [function ({ seriesIndex, dataPointIndex, w }) {
-        //         return w.globals.series[seriesIndex][dataPointIndex]
-        //     }, function ({ seriesIndex, dataPointIndex, w }) {
-        //         var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
-        //         var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
-        //         var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
-        //         var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
-        //         return (
-        //             ''
-        //         )
-        //     }]
-        // },
-        // xaxis: {
-        //     type: 'datetime',
-        // },
-        // annotations: {
-        //     xaxis: [
-        //         {
-        //             x: 'Oct 06 14:00',
-        //             borderColor: '#00E396',
-        //             label: {
-        //                 borderColor: '#00E396',
-        //                 style: {
-        //                     fontSize: '12px',
-        //                     color: '#fff',
-        //                     background: '#00E396'
-        //                 },
-        //                 orientation: 'horizontal',
-        //                 offsetY: 7,
-        //                 text: 'Annotation Test'
-        //             }
-        //         }
-        //     ]
-        // },
-    
     // for (let i = 0; i < data['date'].length - 1; i++) {
     for (let i = data['date'].length - 1; i >= 0; i--) {
         let yr = data['date'][i].split('-')[0];
@@ -93,9 +92,9 @@ export function chartData(data) {
         let high = data['high'][i];
         let low = data['low'][i];
         let close = data['close'][i];
-        let dateObj = new Date(yr, mth - 1, day)
-        let dataObj = { x: (dateObj.getUTCFullYear(), dateObj.getUTCMonth(), dateObj.getUTCDate()),
-            // x: (dateObj.getTime()),
+        let dateObj = new Date(Date.UTC(yr, mth - 1, day, '00', '00', '00'));
+        let dataObj = { 
+            x: (dateObj.getTime()),
             y: [open, high, low, close]
         }
         create.series[0].data.push(dataObj);
